42191eccae0aa75c9bd15d1092990d84f36ef5ce
Sneha4580 <snehakumari4580@gmail.com>
2025-10-27 20:56:37 +0530

    Rebuild and update docs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,29 +1,14 @@
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import "./index.css";
-// import App from "./App.jsx";
-// //import { BrowserRouter } from "react-router";
-// import { HashRouter } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router";
+//import { HashRouter } from 'react-router-dom'
 
-// createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </StrictMode>
-// );
-
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { HashRouter } from 'react-router-dom'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HashRouter>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
       <App />
-    </HashRouter>
-  </React.StrictMode>,
-)
+    </BrowserRouter>
+  </StrictMode>
+);
