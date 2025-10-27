9db3d955a9616241bbe7091f7cde7e924b09bd3e
Sneha4580 <snehakumari4580@gmail.com>
2025-10-27 20:41:30 +0530

    Rebuild and update docs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,29 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router";
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import "./index.css";
+// import App from "./App.jsx";
+// //import { BrowserRouter } from "react-router";
+// import { HashRouter } from 'react-router-dom'
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </StrictMode>
+// );
+
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { HashRouter } from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
       <App />
-    </BrowserRouter>
-  </StrictMode>
-);
+    </HashRouter>
+  </React.StrictMode>,
+)
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/Chatify/',
   plugins: [react()],
+  base: '/Chatify/', // Use lowercase - GitHub Pages URLs are case-sensitive
 })
